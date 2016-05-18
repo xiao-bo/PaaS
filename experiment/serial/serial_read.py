6669e5b4b628ddb8a44d1e802efea9430e21f22d
xiao-bo <ay852852@gmail.com>
2016-05-18 21:32:46 +0800

    can read serial data

---
+++ experiment/serial/serial_read.py
@@ -0,0 +1,11 @@
+import serial
+import datetime
+ser = serial.Serial(
+    port='/dev/tty.usbmodem1421',\
+    baudrate=9600,\
+        timeout=2)
+
+while True:
+	line=ser.readline()
+	print line+str(datetime.datetime.now())
+ser.close()
