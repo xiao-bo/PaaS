2430de4fbb142779f12fb726168104eab6ec37a8
xiao-bo <ay852852@gmail.com>
2016-05-30 19:48:33 +0800

    edison can send data

--- experiment/edison/edison.py
+++ experiment/edison/edison.py
@@ -2,9 +2,11 @@
 import sys
 import time
 import os
+import mraa
+import datetime
 from socket import *
-host = "192.168.0.100" # set to IP address of target computer
-#host = "10.1.2.11" # set to IP address of target computer
+#host = "192.168.0.100" # set to IP address of target computer
+host = "10.8.0.35" # set to IP address of target computer
 port = 13000
 
 class Edison:
@@ -16,18 +18,18 @@ class Edison:
 		TCPSock = socket(AF_INET, SOCK_DGRAM)
 		TCPSock.sendto(str(message),addr)
 		TCPSock.close()
-	"""
+	
 	def analog_read(self):
 		
 		pot=mraa.Aio(0)
 		potVal=float(pot.read())
 		#time.sleep(0.01)
 		return potVal
-	"""
+	
 if __name__=='__main__':
 	client=Edison(host,port)
-	for x in range(1,100):
-		#potVal=client.analog_read()
-		#client.send_message(potVal)
-		client.send_message(x)
+	for x in range(1,10000):
+		potVal=client.analog_read()
+		client.send_message(str(potVal)+' '+str(datetime.datetime.now()))
+		#client.send_message(x)
 
