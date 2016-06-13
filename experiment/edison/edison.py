a3b26bf0662de568cbe3a8a820c8af9dc7492581
xiao-bo <ay852852@gmail.com>
2016-06-13 18:13:55 +0800

    update

--- experiment/edison/edison.py
+++ experiment/edison/edison.py
@@ -4,11 +4,15 @@ import time
 import os
 import mraa
 import datetime
+import threading
 from socket import *
+from math import *
 #host = "192.168.0.100" # set to IP address of target computer
 host = "10.8.0.35" # set to IP address of target computer
+#host="10.1.2.13"
 port = 13000
 
+'''	
 class Edison:
 	def __init__(self,host,port):
 		self.host=host
@@ -24,17 +28,38 @@ class Edison:
 		pot=mraa.Aio(0)
 		potVal=float(pot.read())
 		return potVal
-	
 if __name__=='__main__':
 	client=Edison(host,port)
-	#for x in range(1,10000):
-        fo=open("data.txt","wb")
+        addr=(host,port)
         while True:
-			potVal=client.analog_read()
+	    potVal=client.analog_read()
             message=str(potVal)+' '+str(datetime.datetime.now())
-            client.send_message(message)
-            fo.write(message+'\n')
+            #client.send_message(message)
+	    TCPSock = socket(AF_INET, SOCK_DGRAM)
+	    TCPSock.sendto(message,addr)
             print message
-            time.sleep(0.018)	
-		TCPSock.close()
-		fo.close()
+            #time.sleep(0.018)	
+        TCPSock.close()
+	fo.close()
+'''
+lb=(sin(1020*pi/1000)+2.5)*1024/5
+ub=(sin(980*pi/1000)+2.5)*1024/5
+lb_i=floor(lb)
+ub_i=floor(ub)
+addr=(host,port)
+TCPSock = socket(AF_INET, SOCK_DGRAM)
+while True:
+    #thread.start_new_thread(Thread_read,)
+    #thread.start_new_thread(send_message,)
+    
+    pot=mraa.Aio(0)
+    date_time=str(datetime.datetime.now())
+    potVal=float(pot.read())
+    #print potVal
+    if  ub_i>=potVal and potVal >=lb_i:
+        message=str(potVal)+' '+date_time
+        #print message
+        TCPSock.sendto(message,addr)
+        time.sleep(0.010)	
+    
+TCPSock.close()
