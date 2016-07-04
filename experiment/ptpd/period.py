efea3ebac19eb5525f1a523347de5c1d572a9688
xiao-bo <ay852852@gmail.com>
2016-07-05 02:46:08 +0800

    ptp/period can work

--- experiment/ptpd/period.py
+++ experiment/ptpd/period.py
@@ -1,17 +1,17 @@
 
 import sys
-sys.path.insert(0,'/Users/xiao/Documents/code/paas/git/PaaS/experiment')
-import draw
 def period(filename):
 	readline=open(filename,"rb")
 	offset=[]
 	for x in readline:
-		ans=x.split(":")[1]
+		ans=x.replace(" ","")
+		
+		ans=ans.split(":")[1]
 		ans=ans.split("s")[0]
-		offset.append(ans)
+		offset.append(float(ans))
 	return offset
 
 
 if __name__=="__main__":
 	offset=period("data.txt")
-	draw.ptp_curve(offset)
+	print offset
