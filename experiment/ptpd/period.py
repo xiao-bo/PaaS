47bc72f098414b7537313353ca46ed9b0383109b
xiao-bo <ay852852@gmail.com>
2016-07-08 12:29:42 +0800

    merge

--- experiment/ptpd/period.py
+++ experiment/ptpd/period.py
@@ -1,10 +1,7 @@
 
 import sys
-<<<<<<< HEAD
-=======
 sys.path.insert(0,'/home/newslab/Desktop/PaaS/experiment')
 import draw
->>>>>>> 6bd1f662aa06819929c5e35cb578604aed6429dd
 def period(filename):
 	readline=open(filename,"rb")
 	offset=[]
