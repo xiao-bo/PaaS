ebd5f99874a467b5a49ff96027a2ed0c40ab483e
xiao-bo <ay852852@gmail.com>
2016-07-08 12:22:10 +0800

    edison server can receive

--- experiment/ptpd/period.py
+++ experiment/ptpd/period.py
@@ -1,10 +1,7 @@
 
 import sys
-<<<<<<< HEAD
-=======
 sys.path.insert(0,'/home/newslab/Desktop/PaaS/experiment')
 import draw
->>>>>>> 6bd1f662aa06819929c5e35cb578604aed6429dd
 def period(filename):
 	readline=open(filename,"rb")
 	offset=[]
@@ -12,7 +9,7 @@ def period(filename):
                 tmp=x.replace(" ","")
                 tmp=tmp.split(":")[1]
                 tmp=tmp.split("s")[0]
-		offset.append(float(tmp)*1000)
+		offset.append(float(tmp))
 	return offset
 
 
