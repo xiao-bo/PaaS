d40f0ac752c8bb592c6bb24649506746df7c7780
xiao-bo <ay852852@gmail.com>
2016-07-09 14:51:44 +0800

    merge

--- experiment/serial/serial_read.py
+++ experiment/serial/serial_read.py
@@ -3,13 +3,17 @@
 import serial
 import datetime
 import time
+import subprocess 
+cmd=["ksh -c 'printf \"%(%s.%N)T\"'"]
 ser = serial.Serial(
     port='/dev/tty.usbmodem1411',\
     baudrate=115200,)
 fo=open("old.txt","wb")
 while True:
 	line=ser.readline()
-	ans=line+' '+str(datetime.datetime.now())+' '
+	rece_time=subprocess.check_output(cmd,shell=True)
+	#ans=line+' '+str(datetime.datetime.now())+' '
+	ans=line+' '+str(rece_time)+' '
 	print ans
 	fo.write(ans+'\n')
 	#time.sleep(0.05)
