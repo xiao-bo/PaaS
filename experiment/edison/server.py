8dc3199139641b1933c967799e7da2837f97806b
xiao-bo <ay852852@gmail.com>
2016-07-17 20:44:59 +0800

    edison rece can reuse address without waiting time

--- experiment/edison/server.py
+++ experiment/edison/server.py
@@ -6,9 +6,9 @@ import datetime
 import subprocess
 import sys
 host = ""
-port = 13000
+port = int(sys.argv[2])
 buf = 1024
-filename=sys.argv[1]+"%.txt"
+filename=sys.argv[1]+".txt"
 cmd=["cat /var/run/ptpd2.status.log|grep 'Offset'"]
 cmd2=["ksh -c 'printf \"%(%s.%N)T\'"]
 
@@ -17,27 +17,30 @@ class server:
 		self.port=port
 		self.host=host
         
-	def receive(self):
-		print filename
-        addr=(host,port)
-        Sock = socket(AF_INET, SOCK_STREAM)
-        #Sock.setsockopt(socket.SOL_SOCKET,socket.SO_REUSEADDR,1)
-        Sock.bind(addr)
-        Sock.listen(5)
-        print "Waiting to receive messages..."
-        fo=open(filename,"wb")
-        while True:
-            (csock,adr)= Sock.accept()
-            send_time=csock.recv(10240)
-            rece_time=subprocess.check_output(cmd2,shell=True) 
-            #send_time=data.split(':')[2]## let send time +offset
-            ans=str(send_time)+": mac time:rece_time:"+str(rece_time)
+	def receive(self,filename):
+	    print filename
+            addr=(host,port)
+            Sock = socket(AF_INET, SOCK_STREAM)
+            #Sock.setsockopt(socket.SOL_SOCKET,socket.SO_REUSEADDR,1)
+            Sock.setsockopt(SOL_SOCKET,SO_REUSEADDR,1)
 
-            print float(rece_time)-float(send_time)
-            print ans
-            fo.write(ans+'\n')
-        csock.close()
-        fo.close()
+            Sock.bind(addr)
+            Sock.listen(5)
+            print "Waiting to receive messages..."
+            fo=open(filename,"wb")
+            while True:
+                (csock,adr)= Sock.accept()
+                send_time=csock.recv(10240)
+                rece_time=subprocess.check_output(cmd2,shell=True) 
+                #send_time=data.split(':')[2]## let send time +offset
+                re_time=datetime.datetime.now()
+                ans=str(send_time)+": mac time:rece_time:"+str(rece_time)
+
+                print float(rece_time)-float(send_time)
+                print ans
+                fo.write(ans+'\n')
+            csock.close()
+            fo.close()
 	
 		
 
@@ -45,5 +48,5 @@ if __name__=="__main__":
     
     
     print "sss"
-    #macbook=server(port,host,filename)
-    #macbook.receive(filename)
+    macbook=server(port,host)
+    macbook.receive(filename)
