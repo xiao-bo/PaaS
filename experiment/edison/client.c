205dada23b482d270bcdb9d608e5a21bc5454680
xiao-bo <ay852852@gmail.com>
2016-08-09 02:10:25 +0800

    add comment about edison server and client

--- experiment/edison/client.c
+++ experiment/edison/client.c
@@ -23,16 +23,20 @@ int main(){
     mraa_aio_context adc_a0;
     uint16_t adc_value = 2; //assign pin value
 	struct timeval tv1;// time value
-    int sockfd, portno, n;
+    int sockfd, portno;
     struct sockaddr_in serv_addr;
     struct hostent *server;
-    char buffer[1024];
+    char message[1024];
+    char tmp[100];
 
+	// assign server ip address and port
     portno = 11000;
     server = gethostbyname("192.168.11.8");
-	adc_a0 = mraa_aio_init(2);//assign ping value
+	
+	//assign ping value
+	adc_a0 = mraa_aio_init(2);
     
-	// initial socket
+	// initial property of socket 
     bzero((char *) &serv_addr, sizeof(serv_addr));
     serv_addr.sin_family = AF_INET;
     serv_addr.sin_port = htons(portno);
@@ -44,21 +48,29 @@ int main(){
         return 1;
     }
 
-    char tmp[100];
-	char point[1]=".";
+	// build socket connection
 	sockfd = socket(AF_INET, SOCK_STREAM, 0);
 	connect(sockfd,(struct sockaddr *) &serv_addr,sizeof(serv_addr));
-	while(1){
-        adc_value = mraa_aio_read(adc_a0);
-		if (adc_value==1023){
-			gettimeofday(&tv1,NULL);
-			//sprintf(buffer,"%d",tv1.tv_sec);
-			//sprintf(tmp,"%d",tv1.tv_usec);
 
-			//strcat(buffer,point);
-			//strcat(buffer,tmp);
-			//write(sockfd,buffer,strlen(buffer));
-			write(sockfd,point,1);
+	while(1){
+		// read analog value
+        adc_value = mraa_aio_read(adc_a0);
+
+		if (adc_value==1023){
+			// get current time 
+			gettimeofday(&tv1,NULL);
+
+			// transfer integer of time into string
+			sprintf(message,"%d",tv1.tv_sec);
+			sprintf(tmp,"%d",tv1.tv_usec);
+
+			// cascades sec and usec by "." , for example 10.1234567  
+			strcat(message,".");
+			strcat(message,tmp);
+
+			// send message by socket
+			write(sockfd,message,strlen(message));
+
 			usleep(500000);//microsecond
 		}
 	}
--- experiment/edison/server.py
+++ experiment/edison/server.py
@@ -5,26 +5,37 @@ import time
 import datetime
 import subprocess
 import sys
+
+## Initial server ip address and port
 host = "192.168.11.8"
 #host="140.112.28.139"
 port = int(sys.argv[2])
-buf = 1024
+addr=(host,port)
+
+## name of file
 filename=sys.argv[1]+".txt"
-cmd=["cat /var/run/ptpd2.status.log|grep 'Offset'"]
+
 
 if __name__=="__main__":
     
-    
-    print "sss"
-    addr=(host,port)
+    ## initial socket property
     Sock = socket(AF_INET, SOCK_STREAM)
+
+    ## reuse socket immediately 
     Sock.setsockopt(SOL_SOCKET,SO_REUSEADDR,1)
 
+    ## bind the socket to the port and ip address
     Sock.bind(addr)
+
+    ## listen for incoming connections
     Sock.listen(5)
     print "Waiting to receive messages..."
+
     fo=open(filename,"wb")
+
+    ##  build socket connection 
     connection,client_address= Sock.accept()
+    
     while True:
         send_time=connection.recv(60)
         rece_time=time.time()
@@ -32,5 +43,6 @@ if __name__=="__main__":
         #print float(rece_time)-float(send_time)
         print ans
         fo.write(ans+'\n')
+    
     csock.close()
     fo.close()
