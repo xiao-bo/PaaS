524e606205768624cbd29222cdb208d826e0d785
xiao-bo <ay852852@gmail.com>
2017-05-22 09:03:40 +0000

    receive client python data

--- experiment/ethernet/edison/server.py
+++ experiment/ethernet/edison/server.py
@@ -2,20 +2,39 @@
 import os
 from socket import *
 import time
-import datetime
 import subprocess
 import sys
-
+from influxdb import InfluxDBClient
+from datetime import datetime
 ## Initial server ip address and port
 host = "192.168.11.3"
 #host="140.112.28.139"
 #port = int(sys.argv[2])
-port = 11000
+port = 12000
 addr=(host,port)
 
 ## name of file
 filename=sys.argv[1]+".txt"
 
+json_body=[]
+def insertDataIntoDB(value,epochTime):
+    timestamp = datetime.fromtimestamp(float(epochTime)) 
+    ## transform epochTime into 
+    ## Year-Month-Day Hour-minute-second-millisceond
+    print str(value)+"  "+str(timestamp)
+    
+    tmp_json={
+    "measurement":"edison",
+    "time":str(timestamp),
+    "tags":{
+        "host":"edison"
+    },
+    "fields":{
+        "value":int(value)
+        }
+    }
+    json_body.append(tmp_json)
+    client.write_points(json_body)
 
 if __name__=="__main__":
     
@@ -37,13 +56,26 @@ if __name__=="__main__":
     ##  build socket connection 
     connection,client_address= Sock.accept()
     
+    client = InfluxDBClient('localhost', 8086, 'root', 'root', 'example3')
     while True:
-        send_time=connection.recv(60)
-        rece_time=time.time()
-        ans=str(send_time)+": rece_time:%.9f"%rece_time
+        receiveData=connection.recv(200)
+        print "\n"+str(receiveData)
+        ## below for python client
+        sendTime=receiveData.split(":")[1]
+        value=receiveData.split(":")[0]
+        insertDataIntoDB(value,sendTime)
+        
+        ## below for c client
+        #sendTime=receiveData.split(":")[1]
+        #value=receiveData.split(":")[0]
+        #rece_time=time.time()
+        #ans="Value:"+str(value)+" send_Time"+str(sendTime)+": rece_time:%.9f"%rece_time
         #print float(rece_time)-float(send_time)
-        print ans
-        fo.write(ans+'\n')
+        #ans = "Value:"+str(value)+" send Time:"+str(sendTime)
+        #client = InfluxDBClient('localhost', 8086, 'root', 'root', 'example3')
+        #insertDataIntoDB(value,sendTime)
+        #print ans
+        #fo.write(ans+'\n')
     
     csock.close()
     fo.close()
