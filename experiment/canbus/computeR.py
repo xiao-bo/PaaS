2f2fea19724768682937373141709c6a50bd3369
xiao-bo <ay852852@gmail.com>
2018-05-17 13:19:08 +0800

    update computeR for 1.txt , it can split ID=0010,0011,0012 into three R value

--- experiment/canbus/computeR.py
+++ experiment/canbus/computeR.py
@@ -1,24 +1,44 @@
-with open("0left.txt") as f:
-	content = f.readlines()
+def cal(counter):
+    l = len(counter)
+    print l
+    print counter
 
-counter = []
-for x in content:
-    counter.append(int(x.split(":")[3]))
-
-l = len(counter)
-print l
-print counter
-
-R = (l-2)*1000000.0/2
-print R
-tmp = 0.0
-#for x in range(0,l-2):
-x=0
-while x < l-2:
+    R = (l-2)*1000000.0/2
+    print R
+    tmp = 0.0
+    #for x in range(0,l-2):
+    x=0
+    while x < l-2:
 	print counter[x+2] - counter [x]
 	tmp += counter[x+2] - counter [x]
 	#print tmp
 	x=x+2
-print tmp
-R=R/tmp
-print R
+    print tmp
+    R=R/tmp
+    print R
+    return R
+    
+def main():
+    with open("/home/newslab/Desktop/PaaS/experiment/canbus/data/multiSender/1v3/1.txt") as f:
+        	content = f.readlines()
+
+    counterB0 = []
+    counterB1 = []
+    counterB2 = []
+    for x in content:
+        if x.split(":")[1]=='0010':
+            counterB0.append(int(x.split(":")[3]))
+        elif x.split(":")[1]=='0011':
+            counterB1.append(int(x.split(":")[3]))
+        elif x.split(":")[1]=='0018':
+            counterB2.append(int(x.split(":")[3]))
+
+    R0 = cal(counterB0)
+    R1 = cal(counterB1)
+    R2 = cal(counterB2)
+    print "R0:"+str(R0)
+    print "R1:"+str(R1)
+    print "R2:"+str(R2)
+if __name__=='__main__':
+    main()
+
--- experiment/canbus/data_processing.py
+++ experiment/canbus/data_processing.py
@@ -1,100 +1,97 @@
 import sys
-sys.path.insert(0,'/home/newslab/Desktop/PaaS/experiment/ptpd')
-#import period
 import numpy
 import draw
+
 def ReadArduinoFile(filename,index):
-	##read data and append list
-	file_read=open(filename,'r')
-	S0timestamp = []
-        S1timestamp = []
-        S2timestamp = []
-	for line in file_read:
-            ans=line.split(":")
-            if ans[1]=="0010":
-	        S0timestamp.append(float(ans[index]))
-            elif ans[1]=="0011":
-	        S1timestamp.append(float(ans[index]))
-            elif ans[1]=="0012":
-	        S2timestamp.append(float(ans[index]))
-	return S0timestamp,S1timestamp,S2timestamp
+    ##read data and append list
+    file_read=open(filename,'r')
+    S0timestamp = []
+    S1timestamp = []
+    S2timestamp = []
+    for line in file_read:
+        ans=line.split(":")
+        if ans[1]=="0010":
+            S0timestamp.append(float(ans[index]))
+        elif ans[1]=="0011":
+            S1timestamp.append(float(ans[index]))
+        elif ans[1]=="0018":
+            S2timestamp.append(float(ans[index]))
+    
+    return S0timestamp,S1timestamp,S2timestamp
+	
 def ReadEdisonFile(filename,index):
-	##read data and append list
-	file_read = open(filename,'r')
-        Etimestamp = []
-	for line in file_read:
-            ans=line.split(":")
-            Etimestamp.append(float(ans[index]))
-        return Etimestamp
+    ##read data and append list
+    file_read = open(filename,'r')
+    Etimestamp = []
+    for line in file_read:
+        ans=line.split(":")
+        Etimestamp.append(float(ans[index]))
+    return Etimestamp
 def compute_delay(list_1,list_2):
-	delay=[]
-	for first,second in zip(list_1,list_2):
-	    delay.append(abs(first-second)*1000)
-			
-	return delay
+    delay=[]
+    #shift = 0.0000065
+    shift = 0 
+    i = 0
+    for first,second in zip(list_1,list_2):
+	delay.append(abs(first-second-shift*i)*1000)
+	i=i+1
+    return delay
 
 
 def align(time_list):
-	## count number of zero because x can't ++ in 
-	##loop and lead number of loop shorter 
-	count=0
-	for x in range(0,len(time_list)-1): 
-		diff=time_list[x+1]-time_list[x]
-		#print diff
-		if diff<0:
-			diff=diff+60
-		if diff>0.6:
-			for y in range(0,int(round(diff/0.5))-1):
-				count=count+1
+    ## count number of zero because x can't ++ in 
+    ##loop and lead number of loop shorter 
+    count=0
+    for x in range(0,len(time_list)-1): 
+    	diff=time_list[x+1]-time_list[x]
+    	#print diff
+        if diff<0:
+            diff=diff+60
+	if diff>0.6:
+	    for y in range(0,int(round(diff/0.5))-1):
+		count=count+1
 	for x in range(0,len(time_list)-1+count):
-		if time_list[x]==0:
-			continue
-		diff=time_list[x+1]-time_list[x]
-		if diff<0:
-			diff=diff+60
-		if diff>0.6:
-			for y in range(0,int(round(diff/0.5))-1):## insert multiple zero
-				time_list.insert(x+1,0.0)
+	    if time_list[x]==0:
+	    	continue
+	    diff=time_list[x+1]-time_list[x]
+	    if diff<0:
+		diff=diff+60
+	    if diff>0.6:
+		for y in range(0,int(round(diff/0.5))-1):## insert multiple zero
+		    time_list.insert(x+1,0.0)
 
-	return time_list
+    return time_list
 
 def time_sync(edison,offset):
-	sync=[]
-	##offset = slave - master=mac - edison
-	##
-	for x,y in zip(edison,offset):
-			sync.append(x+float(y))
-	return sync
+    sync=[]
+    ##offset = slave - master=mac - edison
+    ##
+    for x,y in zip(edison,offset):
+	sync.append(x+float(y))
+    return sync
 
 def compute_statistics(delay_list):
-	##evalute average , standard deviation, mean square error
-	delay_list=numpy.array(delay_list)
-        '''
-	summary=0.0
-	for x in delay_list:
-		summary = summary+x*x
-	mse=summary/len(delay_list)
-	'''
-	print "mean: "+str(numpy.mean(delay_list))
-	print "standard deviation: "+str(numpy.std(delay_list))
-	#print "mse: "+str(mse)
-	return [numpy.mean(delay_list),numpy.std(delay_list)]
+    ##evalute average , standard deviation, mean square error
+    delay_list=numpy.array(delay_list)
+
+    print "mean: "+str(numpy.mean(delay_list))
+    print "standard deviation: "+str(numpy.std(delay_list))
+    
+    return [numpy.mean(delay_list),numpy.std(delay_list)]
 
 
 def dataProcess(list_former,list_backer):
 
-	### insert integer 0.0 to recover miss part 
-	#timestamp_former=align(list_former)
-	#timestamp_backer=align(list_backer)
-	### compute delay between list
-	#delay=compute_delay(timestamp_former,timestamp_backer)
-	delay=compute_delay(list_former,list_backer)
-	
-	#if delay:## check list is empty
-	#	compute_statistics(delay)
-	return delay
+    ### insert integer 0.0 to recover miss part 
+    #timestamp_former=align(list_former)
+    #timestamp_backer=align(list_backer)
+    ### compute delay between list
+    #delay=compute_delay(timestamp_former,timestamp_backer)
+    delay=compute_delay(list_former,list_backer)
+
+    return delay
 	
 
 if __name__=='__main__':
-        error_data=dataProcess()
-        draw.curve(error_data,"s")
+    error_data=dataProcess()
+    draw.curve(error_data,"s")
